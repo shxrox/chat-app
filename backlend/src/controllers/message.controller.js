f482196ada7f376d2d0a8f6addbd5e05e9bc4fa9
shxrox <sharondeva8@gmail.com>
2025-11-13 13:00:02 +0530

    feat: implement sendMessage functionality to handle message sending with optional image upload

--- backlend/src/controllers/message.controller.js
+++ backlend/src/controllers/message.controller.js
@@ -30,3 +30,29 @@ export const getMessages = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+export const sendMessage = async (req, res) => { 
+    try{
+        const {text, image} = req.body;
+        const { id: receiverId } = req.params;
+        const senderId =  req.user._id;
+
+        let imageUrl;
+
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const  newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+    }catch(error){
+
+    }
+}
--- backlend/src/routes/message.route..js
+++ backlend/src/routes/message.route..js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { protectRouter } from '../middleware/auth.middleware.js';
-import { getUsersForSidebar , getMessages } from '../controllers/message.controller.js';
+import { getUsersForSidebar , getMessages , sendMessage} from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get('/user',protectRouter, getUsersForSidebar)
 
 router.get('/:id',protectRouter, getMessages);
+
+router.post('/send/:id',protectRouter, sendMessage);
 
 export default router;
